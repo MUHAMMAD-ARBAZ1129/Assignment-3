f1f8b9badfb08e8a888b4a7791cfd0e0798b5c1c
Muhammad Arbaz Latif <arbazchaudhry23@gmail.com>
2019-12-22 01:27:42 +0500

    first commit

---
+++ h.py
@@ -0,0 +1,22 @@
+import turtle
+wn=turtle.screen()
+wn.title("pong by arbaz")
+wn.bdcolor("black")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Main game loop
+while True:
+    wn.update()
